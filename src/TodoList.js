a3d6d66f11d8d7800f9b8c90b6c194fac9d38a7e
Cisse Baker <baker.cisse@gmail.com>
2022-09-27 15:28:46 +0300

    Todolist valmis

---
+++ src/TodoList.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+function TodoList(){
+    const [todo, setTodo] = React.useState({
+        description: '',
+        date: ''
+    });
+    const [todoList, setTodoList] = React.useState([]);
+    
+    const inputChanged = (event) => {
+        setTodo({...todo, [event.target.name] : event.target.value});
+    }
+    
+    const addTodo = (event) => {
+        setTodoList([todo, ...todoList]);
+    }
+    
+    return(
+        <div>
+            <h1>Simple Todo list</h1>
+            Description: <input type="text" name="description" onChange={inputChanged} value={todo.description} />      
+            Date: <input type="text" name="date" onChange={inputChanged} value={todo.date} />
+            <button onClick={addTodo}>Add</button>
+            <table>
+                <tbody>
+                    <tr><th>Date</th><th>Description</th></tr>
+                    {todoList.map((todo, index) => 
+                    <tr key={index}>
+                        <td>{todo.date}</td>
+                        <td>{todo.description}</td>
+                    </tr>)}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default TodoList;
